ee5d8a4dd1be043beee718e0cf202941860d7b7d
khanakitclass <khanakitclass@gmail.com>
2024-08-17 14:36:13 +0530

    dre

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require('cors');
 const routes = require("./routes/api/v1/index");
 const connectDB = require("./db/mongodb");
-const googleProvider = require("./utils/PassportProvider");
+// const googleProvider = require("./utils/PassportProvider");
 
 // const connectMySQLDB = require("./db/mysql");
 // const cookieParser = require('cookie-parser');
