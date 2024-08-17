56d7b470f6b7d61a6e57c753f7fb394a49237759
khanakitclass <khanakitclass@gmail.com>
2024-08-17 14:21:15 +0530

    bb

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ const express = require("express");
 const cors = require('cors');
 const routes = require("./routes/api/v1/index");
 const connectDB = require("./db/mongodb");
-// const googleProvider = require("./utils/PassportProvider");
+const googleProvider = require("./utils/PassportProvider");
 
-// const connectMySQLDB = require("./db/mysql");
-// const cookieParser = require('cookie-parser');
-// const passport = require("passport");
-// const connectChat = require("./utils/socketIO");
-// const swaggerUi = require('swagger-ui-express');
-// const YAML = require('yamljs');
+const cookieParser = require('cookie-parser');
+const passport = require("passport");
+const connectChat = require("./utils/socketIO");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 
 const app = express();
-// const swaggerDocument = YAML.load('./src/api.yaml');
+const swaggerDocument = YAML.load('./src/api.yaml');
 
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
@@ -30,15 +29,13 @@ app.use(cors({
 
 app.use(express.json());
 // app.use(cookieParser())
-// app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 connectDB();
-// googleProvider();
-// connectChat();
-
-// connectMySQLDB();
+googleProvider();
+connectChat();
 
 //localhost:8000/api/v1
 app.get('/', (req, res) => {
@@ -46,10 +43,10 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/v1", routes);
-// app.use((err, req, res, next) => {
-//     console.error(err.stack);
-//     res.status(500).send('Something broke!');
-// });
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 //localhost:8000
 app.listen(8000, () => {
